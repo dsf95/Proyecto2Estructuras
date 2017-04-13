4fde6017d62a2a32b403622f55124b85d207235c
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-04-13 16:19:00 -0600

    Update Gestor.h
    Gestor

--- TorreHanoi/TorreHanoi/Gestor.h
+++ TorreHanoi/TorreHanoi/Gestor.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef GESTOR_H
+#define GESTOR_H
+#include "ListaJugadores.h"
+#include "ListaPuntos.h"
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class Gestor {
+public:
+	Gestor();
+	void agregarJugador(string pnombre);
+	string mostrarJugadores();
+	string buscarJugador(int pcodigo);
+//	bool validarSiHayJugadores();
+private:
+	ListaJugadores listaJugadores;
+	/*ListaPuntos listaPuntos;
+	ListaEstXCurso listaEstXCurso;
+	string generateStrMatricula(Curso curso, Estudiante estudiante); */
+};
+#endif // !GESTOR_H
