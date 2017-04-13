0cba53ccded72997511279452569f0c114ac3175
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-04-13 16:24:58 -0600

    Update ListaJugador.h
    NodoJugadores

--- TorreHanoi/TorreHanoi/ListaJugador.h
+++ TorreHanoi/TorreHanoi/ListaJugador.h
@@ -1,1 +1,31 @@
-#pragma once
+#ifndef LISTAJUGADORES_H
+#define LISTAJUGADORES_H
+#include "NodoJugadores.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class ListaJugadores {
+public:
+	ListaJugadores();
+	void setLongitud();
+	int getLongitud();
+	void setCabeza(NodoJugadores*);  
+	NodoJugadores *getCabeza();
+
+	void agregarJugador(Jugador pjugador);
+	bool jugadoresVacio();
+	string mostrarJugadores();
+	string buscarInfoDelJugador(string pjugador);
+	Jugador buscarJugador(string pjugador);
+	bool seEncuentraJugador(string pcodigo);
+	bool listaJugadorVacio();
+	string toUppercase(string pcodigo);
+private:
+	int longitud;
+	NodoJugadores *cabeza;
+};
+#endif // !LISTAJUGADORES_H
+
+
